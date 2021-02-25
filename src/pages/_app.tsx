cacce3967f5bb71212fd49788c55edc0b9486d92
Bruno Souza <bruno.arubesu@gmail.com>
2021-02-25 00:24:40 -0300

    Clear index | _app | global.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import { ExperienceBar } from '../components/ExperienceBar'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="container">
-      <ExperienceBar />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
