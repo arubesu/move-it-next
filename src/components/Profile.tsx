b8294aaf9438fb8b821c79ef8dad72bf5785fc4f
Bruno Souza <bruno.arubesu@gmail.com>
2021-02-24 23:57:03 -0300

    Add Profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Profile: React.FC = () => {
+  return (
+    <div>
+      <img src="https://github.com/arubesu" alt="Bruno Souza" />
+      <div>
+        <strong>Bruno Souza</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
